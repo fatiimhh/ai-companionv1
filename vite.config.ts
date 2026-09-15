520903e53657d537b4d8f074835bb9e32c7473fc
Fatima Bani Sakher <fatima.mbk00@gmail.com>
2026-09-15 19:40:50 +0300

    vite.config.ts updated new server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/ai-companionv1/",
+
+   server: {
+    port: 5173,
+    strictPort: true,
+  },
 })
